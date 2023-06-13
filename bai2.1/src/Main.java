32bab9d88dfeee09cbdabbbbba3641be918d49ed
Chienhandsome <cerkvena291@gmail.com>
2023-06-13 21:23:14 +0700

    chien -4 -2.1

--- bai2.1/src/Main.java
+++ bai2.1/src/Main.java
@@ -1,7 +1,7 @@
 import java.text.Normalizer;
 
 public class Main {
-    public static String removeAccent(String s) {
+    public static     String removeAccent(String s) {
         String str = Normalizer.normalize(s, Normalizer.Form.NFD);
         str = str.replaceAll("\\p{M}", "");
         return str;
